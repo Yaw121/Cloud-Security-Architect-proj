f77b9dfef7425c6d6db8521b73fffb7fe80da914
Emmanuel Nyamekye <nyamekyee84@gmail.com>
2026-05-04 14:44:21 -0400

    fix gitignore typo and update modules output

--- modules/output.tf
+++ modules/output.tf
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 output "public-ip-address" {
   value = aws_instance.mylab.public_ip
 }
+=======
+output "public-ip-address" {
+  value = aws_instance.mylab.public_ip
+}
+>>>>>>> da026ed (fix gitignore typo and update modules output)
